0796e311d4145276043dd1dd41c6afa6d9629fd0
Jake Schaap <jakeschaap@gmail.com>
2022-10-20 06:01:16 -0400

    new loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,10 +1,7 @@
 <script>
-import { Circle10 } from 'vue-loading-spinner';
+// import { Circle10 } from 'vue-loading-spinner';
 export default {
-    name: 'Loader',
-    components: {
-        Circle10
-    }
+    name: 'Loader'    
 }
 </script>
 
@@ -12,15 +9,44 @@ export default {
     <div id="loader-container" class="mt-6">
         <p class="text-2xl text-center">We are calculating your custom macro results</p>
 
-        <circle10></circle10>
+        <div class="circ" style="--color: rgb(111, 140, 222); --size: 150px;">
+            <div class="circ" style="--color: rgb(248, 10, 105); --size: 125px;">
+                <div class="circ" style="--color: white; --size: 100px;"> </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    .spinner.spinner--circle-10 {
+    /* .spinner.spinner--circle-10 {
         height: 100px !important;
         width: 100px !important;
         margin: 40px auto 0;
         transform: scale(2);
-    }    
+    }     */
+
+    
+
+    .circ {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 50%;
+        border-bottom: 8px var(--color) solid;
+        height: var(--size);
+        width: var(--size);
+
+        animation: rotation 2s linear infinite;
+    }
+
+    @keyframes rotation {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
 </style>
